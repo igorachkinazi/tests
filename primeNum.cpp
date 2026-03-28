9a9e6736096a405b85c5482aa01f35da12e10643
centos <maxim.achkinazi@gmail.com>
2026-03-28 15:11:17 -0400

    PrimeNum updated

--- primeNum.cpp
+++ primeNum.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 bool isPrime(int n);
+void printInternalDivisors(int n);
 
 int main() {
     cout << "Hello from primeNum!" <<endl;
@@ -13,9 +16,36 @@ int main() {
         cout << num << " is a prime number." << endl;
     } else {
         cout << num << " is not a prime number." << endl;
+        cout << "Divisors of " << num << " (excluding 1 and itself): ";
+        printInternalDivisors(num);
+        cout << endl;
     }
+
     return 0;
 }
+
+// Print all divisors of n except 1 and n itself.
+void printInternalDivisors(int n) {
+    if (n <= 3) {
+        cout << "(none)";
+        return;
+    }
+
+    
+    int limit = n / 2; // No need to check beyond n/2;
+
+    bool first = true; // To handle comma placement
+    for (int i = 2; i <= limit; i++) {
+        if (n % i == 0) {
+            if (!first) {
+                cout << ", ";
+            }
+            cout << i;
+            first = false;
+        }
+    }
+}
+
 // Write a C++ function that checks if a given number is prime or not. The function should take an integer as input and return a boolean value indicating whether the number is prime.
 bool isPrime(int n) {
     if (n <= 1) {
